c91b63c4ccf1d1d9a4bf8fba935c2925dbed229c
Volodymyr <zumnuj@gmail.com>
2022-08-15 16:09:33 +0300

    send message, scroll

--- src/js/script.js
+++ src/js/script.js
@@ -124,4 +124,18 @@ $('form').submit(function(e) {
   });
   return false;
 });
-  
+
+//scroll and pageup
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 1200) {
+    $('.pageup').fadeIn();
+  } else {
+    $('.pageup').fadeOut();
+  }
+});
+
+$("a[href^='#']").click(function(){
+  const _href = $(this).attr("href");
+  $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+  return false;
+});
